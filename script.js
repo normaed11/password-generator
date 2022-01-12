e4069646afa340b6efc252f80914f0c0b37e74e5
normadiaz <normadiazchb@me.com>
2022-01-12 13:03:10 -0600

    correct html and js link

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const includelowercase = document.getElementById('includelowercase')
 const includenumbers = document.getElementById('includenumbers')
 const includesymbols = document.getElementById('includesymbols')
 const form = document.getElementById('passwordgeneradorform')
+console.log(
+    "hello"
+)
 const passwordDisplay = document.getElementById('passwordDisplay')
 const includeuppercase = Arrayfrom('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 const includelowercase = Arrayfrom('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
@@ -22,6 +25,16 @@ form.addEventListener('submit', e => {
     const includelowercase = includelowercaseElement.checked
     const includenumbers = includenumbersElement.checked
     const includesymbols = includeuppersymbolsElement.checked
+    console.log(
+        CharatersAmount,
+        includelowercase,
+        includeuppercase,
+        includenumbers,
+        includesymbols
+    )
+
+
+
     const password = generatepassword(CharatersAmount, includeuppercase, includelower,
         includenumbers, includesymbols)
     passwordDisplay.innerText = password
